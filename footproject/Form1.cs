05f7b0c06a27b63fb64e37fe2bba94991fb8cf0b
Wei-9906 <lkjh11155038@lkjh.tp.edu.tw>
2025-02-18 15:56:20 +0800

    2025/02/18

---
+++ footproject/Form1.cs
@@ -0,0 +1,20 @@
+namespace footproject
+{
+    public partial class Form1 : Form
+    {
+        public Form1()
+        {
+            InitializeComponent();
+        }
+
+        private void button1_Click(object sender, EventArgs e)
+        {
+            textBox1.Text = "UWU";
+        }
+
+        private void textBox1_TextChanged(object sender, EventArgs e)
+        {
+
+        }
+    }
+}
